d7ba3064e20be772643f603d39540ef5b430bb3c
Davin Karunanayake <davikaru123@gmail.com>
2024-09-15 13:50:14 +1000

    1. Updated landscape layout to look prettier with better postioned elements 2. Application now saves state when moving between portrait and landscape

--- app/src/main/java/au/edu/swin/sdmd/l03_manyimages/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/l03_manyimages/MainActivity.kt
@@ -1,7 +1,6 @@
 package au.edu.swin.sdmd.l03_manyimages
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
@@ -12,6 +11,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var station: Button
     private lateinit var college: Button
     private lateinit var theatre: Button
+
+    private var selectedImageResId: Int = R.drawable.station
+    private var selectedButtonId: Int = R.id.station
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,20 +28,40 @@ class MainActivity : AppCompatActivity() {
         college.backgroundTintList = null
         theatre.backgroundTintList = null
 
+        if (savedInstanceState != null) {
+            selectedImageResId = savedInstanceState.getInt("selectedImageResId", R.drawable.station)
+            selectedButtonId = savedInstanceState.getInt("selectedButtonId", R.id.station)
+        }
+
+        image.setImageDrawable(getDrawable(selectedImageResId))
+        updateButtonState(findViewById(selectedButtonId))
+
         station.setOnClickListener {
             image.setImageDrawable(getDrawable(R.drawable.station))
             updateButtonState(station)
+            selectedImageResId = R.drawable.station
+            selectedButtonId = R.id.station
         }
 
         college.setOnClickListener {
             image.setImageDrawable(getDrawable(R.drawable.college))
             updateButtonState(college)
+            selectedImageResId = R.drawable.college
+            selectedButtonId = R.id.college
         }
 
         theatre.setOnClickListener {
             image.setImageDrawable(getDrawable(R.drawable.theatre))
             updateButtonState(theatre)
+            selectedImageResId = R.drawable.theatre
+            selectedButtonId = R.id.theatre
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("selectedImageResId", selectedImageResId)
+        outState.putInt("selectedButtonId", selectedButtonId)
     }
 
     private fun updateButtonState(selectedButton: Button) {
